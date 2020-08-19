d805c6e2028c488f633265ea8f42dcec1a1b8110
Sumit Nangia <sumit.nangia37@gmail.com>
2020-08-19 16:58:35 +0530

    Add files via upload

---
+++ src/Day3assign/Switch.java
@@ -0,0 +1,26 @@
+package Day3assign;
+
+public class Switch {
+
+    public static void main(String[] args) {
+        int i= 6;
+	String Month="";
+        switch (i)
+        {
+            case 1:
+                Month = "January";
+                break;
+            case 2:
+                Month="February";
+                break;
+            case 3:
+                Month="March";
+                break;
+            case 4:
+                Month="April";
+            default:
+                Month="None";
+        }
+        System.out.println(Month);
+    }
+    }
