8bb9615b2efe382046a7505062af0ce237bc7aba
Sumit <sumit.nangia37@gmail.com>
2020-08-24 01:59:30 +0530

    Added Integers till 20

---
+++ src/Day3assign/IntegersTill20.java
@@ -0,0 +1,11 @@
+package Day3assign;
+
+public class IntegersTill20 {
+    public static void main(String[] args) {
+        System.out.println("Here Goes list of Integers");
+        for(int i =0; i<=20;i++){
+            System.out.println(i);
+        }
+    }
+}
+
