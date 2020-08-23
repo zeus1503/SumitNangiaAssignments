a776bd2387e0234b3ded05568013f3949223a98c
Sumit <sumit.nangia37@gmail.com>
2020-08-24 01:55:48 +0530

    Added table of 6 code

---
+++ src/Day3assign/TableofSix.java
@@ -0,0 +1,12 @@
+package Day3assign;
+
+public class TableofSix {
+
+    public static void main(String[] args) {
+        System.out.println("Here Goes table of 6");
+        int val =6;
+        for(int i=1; i<=10;i++){
+            System.out.println(val+"X" + i +"="+val*i);
+        }
+    }
+}
