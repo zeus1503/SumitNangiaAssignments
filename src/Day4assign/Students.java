10e09a501115f14bfac48745dbb40566db7f542f
Sumit <sumit.nangia37@gmail.com>
2020-08-24 20:05:55 +0530

    Updated Code for Students Class. Day 4

--- src/Day4assign/Students.java
+++ src/Day4assign/Students.java
@@ -1,4 +1,20 @@
 package Day4assign;
+import java.lang.Math;
 
 public class Students {
+
+    public  void StudentDetail(int roll, String nm){
+            int SeatNumber = roll+10;
+            int rank = (int) (Math.random()*10);
+            System.out.println("Seat Number for "+ nm + " is " + SeatNumber);
+            System.out.println("Seating Position for " + nm + " is " + rank);
+        }
+
+    public static void main(String[] args) {
+
+        Students a = new Students();
+        a.StudentDetail(20,"Mohit");
+    }
+
+
 }
